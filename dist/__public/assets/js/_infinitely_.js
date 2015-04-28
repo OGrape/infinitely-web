6dcb216f23b7f8809f5b7e84c9b760020207ee1a
Sam Ng <sam@devstudio.io>
2015-04-27 22:54:05 -0600

    Add Google Analytics

--- dist/__public/assets/js/_infinitely_.js
+++ dist/__public/assets/js/_infinitely_.js
@@ -1229,4 +1229,12 @@ Y.prototype.load=function(a){a(this.o)};function Z(a,b){this.d=a;this.f=b}Z.prot
 
 	m.module(document.querySelector('#section-home-contact ul'), Contact);
 
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-62215189-2', 'auto');
+	ga('send', 'pageview');
+
 })(window || this);
